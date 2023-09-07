701c9be42a2a1ad7682e68bc9cd9cb52208b6555
Alex <alexrkoch@gmail.com>
2023-09-06 21:37:13 -0400

    add videos list

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+mod video_data;
+use video_data::get_videos;
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
+    let videos = get_videos();
+
+    let videos_html = videos
+        .iter()
+        .map(|video| {
+            html! {
+                <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+            }
+        })
+        .collect::<Vec<_>>();
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                { for videos_html }
+            </div>
+        </>
     }
 }
 
